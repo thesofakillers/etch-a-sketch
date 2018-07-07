40f9570f3723b25ecf408415383c50c45a7c1fae
Giulio Starace <giulio.starace@gmail.com>
2018-07-07 13:01:32 +0200

    bind the resetting of the sketch area to the reset button by encapsulating it in a function triggered via a click event listener

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-var size = prompt("Enter the size N of the N x N grid you wish to create: ");
+function resetSize(){
+  //clears the work area
+  var cells = document.getElementsByClassName('gridCell');
+  while(cells[0]) {
+      cells[0].parentNode.removeChild(cells[0]);
+  }
 
-var sketchArea = document.querySelector("div.sketchArea");
-
-var cellWidth = 100/size
-var gridCell;
-
-for (i = 0; i < (size*size); i++){
-  gridCell = document.createElement('div');
-  gridCell.setAttribute('class', 'gridCell');
-  gridCell.style.width = cellWidth.toString()+"%";
-  gridCell.style.height = cellWidth.toString()+"%";
-  sketchArea.appendChild(gridCell)
+  //fills the work area
+  var size = prompt("Enter the size N of the N x N grid you wish to create: ");
+  var sketchArea = document.querySelector("div.sketchArea");
+  var cellWidth = 100/size
+  var gridCell;
+  for (i = 0; i < (size*size); i++){
+    gridCell = document.createElement('div');
+    gridCell.setAttribute('class', 'gridCell');
+    gridCell.style.width = cellWidth.toString()+"%";
+    gridCell.style.height = cellWidth.toString()+"%";
+    sketchArea.appendChild(gridCell)
+  }
 }
+
+const resetButton = document.querySelector("button#Reset");
+resetButton.addEventListener("click", resetSize);
